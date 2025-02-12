f81885fc1459fcc6ea6c3a12b6de02f97df01f68
Brenno <119629292+brennobranco@users.noreply.github.com>
2025-02-12 16:40:21 -0300

    feature: componente de input do formulário

---
+++ front/src/components/InputsDeFormulario/style.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    width: 100%;
+    flex-direction: column;
+    gap: 8px;
+`
+export const InputContainer = styled.View`
+    width: 100%;
+    gap: 10px;
+    flex-direction: row;
+    padding: 10px 0px;
+    align-items: center;
+
+    //margin: 0px 10px 10px 10px;
+    border-bottom-width: 1.5px;
+    border-bottom-color: rgba(5, 61, 88, 0.80);
+`
+
+export const InputEstilizado = styled.TextInput`
+    width: 100%;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: rgba(6, 7, 19, 0.40);
+    outline-style: none;
+`
+
+export const ImagemEstilizada = styled.Image`
+    width: 24px;
+    height: 24px;
+`
+
+export const TextoEstilizado = styled.Text`
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: #154962;
+    text-decoration: underline;
+`
