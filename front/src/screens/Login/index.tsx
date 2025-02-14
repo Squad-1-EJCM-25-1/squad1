a35f23b5b9573a6d6eddcba61711b20aa5a500a4
Brenno <119629292+brennobranco@users.noreply.github.com>
2025-02-14 16:06:32 -0300

    feature: Criação dos formulários de login e cadastro

---
+++ front/src/screens/Login/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { BotaoContainer, FormularioContainer, Imagem, InputContainer, Logo, SenhaEsquecidaContainer, Tela } from './style'
+import Botao from '../../components/botões'
+import Input from '../../components/InputsDeFormulario'
+
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+
+const schema = yup.object({
+  email: yup.string().email("Email Inválido").required("Informe seu email"),
+  senha: yup.string().min(6, "A senha deve ter pelo menos 6 dígitos").required("Informe sua senha")
+})
+
+const Login = () => {
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const SubmeterFormulario = (data: any) => {
+    console.log(data)
+  }
+
+  return (
+    <Tela>
+      <FormularioContainer>
+
+        <Logo
+          source={require('../../assets/logo.png')}
+        />
+
+        <Imagem
+          source={require('../../assets/patasCaninas.png')}
+        />
+
+        <InputContainer>
+
+          <Input
+            control={control}
+            name="email"
+            textoDePreenchimento="Digite seu email"
+            srcImg={require('../../assets/iconesDosInputs/gatinho.png')}
+            texto='Login'
+            error={errors.email?.message}
+            inputSeguro={false}
+          />
+
+          <Input
+            control={control}
+            name="senha"
+            textoDePreenchimento="Digite sua senha"
+            srcImg={require('../../assets/iconesDosInputs/cadeado.png')}
+            texto='Senha'
+            error={errors.senha?.message}
+            inputSeguro={true}
+          />
+
+        </InputContainer>
+
+        <SenhaEsquecidaContainer
+          style={{
+            textShadowOffset: { width: 0, height: 4 },
+            textShadowRadius: 4,
+            textShadowColor: "rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          Esqueceu a senha?
+        </SenhaEsquecidaContainer>
+
+        <BotaoContainer>
+          <Botao
+            corDeFundo='laranja'
+            texto='Entrar'
+            aoApertar={handleSubmit(SubmeterFormulario)}
+          />
+          <Botao
+            corDeFundo='laranja'
+            texto='Cadastrar'
+            aoApertar={() => console.log("oi")} // apagar
+          />
+        </BotaoContainer>
+      </FormularioContainer>
+    </Tela>
+  )
+}
+
+export default Login
---
+++ front/src/screens/Login/style.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components/native";
+
+export const Tela = styled.View`
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    background-color: #E6E8E9;
+    padding: 0px 35px 0px 41px;
+`
+
+export const FormularioContainer = styled.View`
+    width: 100%;
+    align-items: center;
+    background-color: none;
+`
+
+export const InputContainer = styled.View`
+    width: 100%;
+    align-items: center;
+    gap: 8px;
+`
+
+export const SenhaEsquecidaContainer = styled.Text`
+    width: 100%;
+    text-align: end;
+
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: #154962;
+`
+
+export const BotaoContainer = styled.View`
+    width: 100%;
+    align-items: center;
+    gap: 12px;
+    margin-top: 90px;
+`
+
+export const Logo = styled.Image`
+    width: 195px;
+    height: 84px;
+`
+
+export const Imagem = styled.Image`
+    width: 198px;
+    height: 194px;
+`
