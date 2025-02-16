fee4fcb2434ec6a21687bdbe9f16ab90d3a822a0
Bruno Schulz <brunovschulz@gmail.com>
2025-02-16 16:25:24 -0300

    Funções para deletar e pegar um usuário específico

--- back/src/controllers/usuarioController.ts
+++ back/src/controllers/usuarioController.ts
@@ -41,6 +41,66 @@ class usuarioController {
 
     }
 
+    public async getUsuario(req: Request, res: Response){
+
+        const {idUsuario} = req.params
+
+        try {
+            
+            const usuario = prisma.usuario.findUnique({
+                where:{idUsuario: Number(idUsuario)}
+            })
+
+            if(!usuario){
+                res.status(404).json({message: "Usuário não encontrado!"})
+                return;
+            }
+
+            if(!usuario.ehVendedor){
+
+                const cliente = prisma.cliente.findUnique({
+                    where:{idCliente: Number(idUsuario)}
+                })
+
+                res.status(200).json({message:"Cliente encontrado com sucesso!", data:{usuario, cliente}});
+
+            } else{
+
+                const vendedor = prisma.vendedor.findUnique({
+                    where:{idVendedor: Number(idUsuario)}
+                })
+
+                res.status(200).json({message:"Vendedor encontrado com sucesso!", data:{usuario, vendedor}});
+            }
+
+        } catch (error: any) {
+            
+            res.status(500).json({error: error.message})
+        }
+
+    }
+
+// apagando instância de usuário, estaremos apagando instância de
+// vendedor e cliente, uma vez que a chave estrangeira possui onDelete Cascade
+    public async deletarUsuario(req:Request, res: Response){
+
+        const {idUsuario} = req.params;
+        
+        try {
+            
+            const usuario = prisma.usuario.delete({
+                where:{idUsuario: Number(idUsuario)}
+            })
+
+            res.status(200).json({message:"Cliente deletado com sucesso!", usuario: usuario})
+
+        } catch (error: any) {
+            
+            res.status(500).json({message:error.message})
+        }
+
+    }
+
     public async login(req: Request, res: Response){
 
         const { email, senha } = req.body;
--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -13,9 +13,13 @@ const router = Router();
 // se precisarmos do token em alguma rota, utilizar 
 // passport.authenticate("jwt", {session: false}) como middleware da rota
 
-router.post("/usuario", usuarioController.criarCliente)
+router.post("/usuario", usuarioController.criarCliente);
 
-router.get("/login", usuarioController.login)
+router.delete("/usuario/:idUsuario", usuarioController.deletarUsuario);
+
+router.get("/usuario/:idUsuario", usuarioController.getUsuario);
+
+router.get("/login", usuarioController.login);
 
 
 export default router;
