4570e54a261f71bc5e49faadc80d0fb4beae87aa
NathanSecundesPereira <nathan.secundespereira@gmail.com>
2025-02-20 22:20:02 -0300

    Update seeder.ts

--- back/database/seeder.ts
+++ back/database/seeder.ts
@@ -1,3 +1,5 @@
+//usuario main seeder
+
 import { PrismaClient } from "@prisma/client";
 import {usuarioseed} from "./models/usuarioseed"
 const prisma = new PrismaClient()
@@ -5,6 +7,27 @@ const prisma = new PrismaClient()
 async function main () {
 
     await usuarioseed()
+}
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+        
+    })
+
+//pet main seeder
+
+import { PrismaClient } from "@prisma/client";
+import {petseed} from "./models/petseed"
+const prisma = new PrismaClient()
+
+async function main () {
+
+    await petseed()
 }
 main()
     .then(async () => {
